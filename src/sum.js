26661fd75f1a4ae1b19d99b762b039551f7fca4d
josephkipkemoi <jkemboe@gmail.com>
2025-01-17 17:47:15 +0000

    sum test refactor

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,11 @@
-function sum(a,b) {
+ function sum(a,b) {
     return a + b
 }
+
+/*
+    ES-Module
+    This is a mechanism for splitting Javascript programs into seperate modules that can be imported when needed
+    The first thing you do to get access to module features is to export them, this is done using the export statement
+*/
 
 module.exports = sum
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,10 @@
-const sum = require("../src/sum")
+const sum = require("../src/sum") 
 
+/* 
+    The test function from jest takes two arguements,
+    the first arguement is a string of what you expect from your test,
+    the second arguement is a calledback function with your test assertions
+*/
 test("adds 10 + 20 to equal 30", () => {
     expect(sum(10,20)).toBe(30)
 })
