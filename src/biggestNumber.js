72f6b8949df1a316b84d0bc6ac6801e71014b82f
josephkipkemoi <jkemboe@gmail.com>
2025-02-12 05:59:24 +0000

    Unit testing for learners

--- src/biggestNumber.js
+++ src/biggestNumber.js
@@ -1,13 +1,13 @@
 const biggestNumber = (arr) => {
-    // Create variable that will store our base number, this will be used to check
-    // against the numbers in the array, we initialize the variable to 0
-   let largestNum = 0
+    // Set the first digit in the array as our initial biggest number, this will be used to check
+    // against the subsequent numbers in the array
+   let largestNum = arr[0]
 
     // Loop through the array in order to query the size of integers     
    for(i = 0; i < arr.length; i++) {
-    // Check current array integer size in comparison with our base number
+    // Check current array integer size in comparison with our largestNumber initiliazer
     if(arr[i] > largestNum) {
-        // If it is greater than our largestNum integer, we put the value of the integer as our base value
+        // If it is greater than our largestNum integer, we put the value of the integer as our largestNumber
         // and the loop goes on till the last digit in the array
         largestNum = arr[i]
     }
--- src/sum.js
+++ src/sum.js
@@ -8,4 +8,17 @@
     The first thing you do to get access to module features is to export them, this is done using the export statement
 */
 
+// function isEvenOrODd(n) {
+//     if (n % 2 != 0) {
+//         console.log("odd")
+//     } else {
+//         console.log("even")
+//     }
+//     n % 2 === 0 ? "even" : "odd"
+// }
+
+// isEvenOrODd(910)
+
+let x = "#"
+let y = "#"
 module.exports = sum
